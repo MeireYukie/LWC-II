6f190ab8ddaa00ec0f90ad26268a34ef4a8e9389
MeireYukie <129764347+MeireYukie@users.noreply.github.com>
2023-04-26 19:06:08 -0300

    Create lwc_II_Exercicio1.js

---
+++ Exercicio 1/lwc_II_Exercicio1.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+export default class Lwc_II_Exercicio1 extends LightningElement {
+
+cliente = [ 
+    {id:1, primeironome:"Thay", sobrenome:"Indiana", idade:"30", email:"thay@gmail.com", classificacao: "hot" , empresa: "Accenture" , receitaanual: 2000000},
+    {id:2, primeironome:"Gui", sobrenome:"Santos", idade:"30", email:"gui@gmail.com", classificacao: "hot" , empresa: "Accenture" , receitaanual: 2000000},
+    {id:3, primeironome:"Matheus", sobrenome:"Dias", idade:"30", email:"mat@gmail.com", classificacao: "hot" , empresa: "Accenture" , receitaanual: 2000000},
+    {id:4, primeironome:"Jessica", sobrenome:"Gomes", idade:"30", email:"je@gmail.com", classificacao: "hot" , empresa: "Accenture" , receitaanual: 2000000},
+    {id:5, primeironome:"Juci", sobrenome:"Dibas", idade:"30", email:"juci@gmail.com", classificacao: "hot" , empresa: "Accenture" , receitaanual: 2000000},
+
+];
+
+}
+
+
+
+
