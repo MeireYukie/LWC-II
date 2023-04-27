3de04b5eca96d3a93ecf67fc2ce0e1e5a33a5909
MeireYukie <129764347+MeireYukie@users.noreply.github.com>
2023-04-27 00:37:17 -0300

    Create exerciciolead.js

---
+++ Exercicio 3/exerciciolead.js
@@ -0,0 +1,20 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Exerciciolead extends LightningElement {
+
+    @api ide;
+    @api firstname;
+    @api lastname;
+    @api company;    
+    @api rating;
+    @api status;
+
+}
+
+
+
+
+
+
+
+
