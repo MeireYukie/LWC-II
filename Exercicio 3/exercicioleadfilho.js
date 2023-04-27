3a4b41356797bb3f988062df3c629e434587de43
MeireYukie <129764347+MeireYukie@users.noreply.github.com>
2023-04-27 00:39:17 -0300

    Create exercicioleadfilho.js

---
+++ Exercicio 3/exercicioleadfilho.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class exercicioleadfilho extends LightningElement {
+
+lead = [
+    {ide:0, firstname:'Meire',     lastname:'Yukie',    company:'Soul Code', rating:'Hot', status:'Open - Not Contacted'},
+    {ide:1, firstname:'Giovanna',  lastname:'Barbosa',  company:'Soul Code', rating:'Hot', status:'Open - Not Contacted'},
+   
+];
+}
+
