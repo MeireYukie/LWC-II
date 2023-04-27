502312d2f806567e34612c6ebca28d00e9797370
MeireYukie <129764347+MeireYukie@users.noreply.github.com>
2023-04-27 00:34:34 -0300

    Create oppsdaconta.js

---
+++ Exercicio 2/oppsdaconta.js
@@ -0,0 +1,14 @@
+import { LightningElement, api } from "lwc" ;
+
+export default class oppsdaconta extends LightningElement {
+    
+    @api ide;
+    @api name;
+    @api amount;
+    @api stagename;
+}
+
+
+
+
+
