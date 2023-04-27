6e8bab721c19866126f308bfb10127db5286ed9f
MeireYukie <129764347+MeireYukie@users.noreply.github.com>
2023-04-27 00:33:17 -0300

    Create propriedadePublica.js

---
+++ Exercicio 2/propriedadePublica.js
@@ -0,0 +1,12 @@
+import { LightningElement } from "lwc" ;
+
+export default class propriedadePublica extends LightningElement {
+
+Opp = [
+    {ide:0, name:'Meire',     amount:5, stagename:'Qualification'},
+    {ide:1, name:'Thayana',   amount:6, stagename:'Qualification'},
+    {ide:2, name:'Matheus',   amount:7, stagename:'Qualification'},
+    {ide:3, name:'Guilherme', amount:8, stagename:'Qualification'},
+    {ide:4, name:'Jessica',   amount:9, stagename:'Qualification'},
+];
+}
